0cb9588ce6d784c4ef73b114282565a7dbbd1b4b
artenlf <felipe.arten@gmail.com>
2022-10-27 00:01:35 -0300

    refactor: remove fragments

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -2,12 +2,10 @@ import styles from "./Input.module.css";
 
 export function Input() {
   return (
-    <>
-      <input
-        className={styles.input}
-        placeholder="Add new task"
-        type="text"
-      ></input>
-    </>
+    <input
+      className={styles.input}
+      placeholder="Add new task"
+      type="text"
+    ></input>
   );
 }
